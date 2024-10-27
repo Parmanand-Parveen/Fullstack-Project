18e7f8688e995624b6b8821be08c13363aee8518
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-27 09:25:42 +0530

    User Controller ✅

--- Backend/controllers/user.control.js
+++ Backend/controllers/user.control.js
@@ -2,13 +2,14 @@ const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const generateToken = require("../services/jwt")
 const jwt = require("jsonwebtoken")
+const dotenv = require("dotenv").config() 
 
 
 const registerUser = async (req,res)=>{
     const {username, password,email} = req.body
     const existingUser =await userModel.findOne({email})
     if(existingUser){
-        res.send("User already exist")
+        res.send("User already exist").json({success:false,message:"User already exist"})
     } else {
         const hashPassword = bcrypt.hash(password,10,async (err,hashedPassword)=>{
             const user = await userModel.create({
@@ -18,10 +19,15 @@ const registerUser = async (req,res)=>{
             })
             
     
-            const token =  jwt.sign({email:user.email,id:user._id},"222",{expiresIn:"1h"})
+            const token =  jwt.sign({email:user.email,id:user._id},process.env.JWT_SECRET_KEY,{expiresIn:"1h"})
             await res.cookie("token",token)
-              res.json(user)
+              res.json({
+                success: true,
+                message: "User created successfully",
+                user:user
+              })
         })
+        
         
 
     }
@@ -30,26 +36,44 @@ const registerUser = async (req,res)=>{
 const loginUser = async (req,res)=>{
     const {email,password} = req.body
     const user = await userModel.findOne({email})
-    console.log(user)
     if(!user){
         res.send("User does not exist")
     } else {
         const match = bcrypt.compare(password,user.password,(err,match)=>{
             if(!match){
                 res.status(400) 
-                res.send(err)
+                res.send("Wrong password")
             }else {
-                const token = jwt.sign({email:user.email,id:user._id},"222",{expiresIn:"1h"})
-                res.cookie("token",token,).json(user)
+                const token = jwt.sign({email:user.email,id:user._id},process.env.JWT_SECRET_KEY,{expiresIn:"1h"})
+                res.cookie("token",token,)-
+                res.json({success:true,user:user})
             
             }
         })
     }
 }
 
-const logoutUser = async (req,res)=>{
-    res.clearCookie("token")
-    res.send("User logged out")
+const logoutUser = async(req,res)=>{
+    res.cookie("token","")
+    res.json({success:true,message:"User logged out"})
+}
+
+
+const checkAuth = async(req,res,next)=>{
+    const token = req.cookies.token
+    console.log(token)
+    if(!token){
+        res.status(400).json({success:false,message:"User not logged in"})
+    }else{
+        const verifiedUser = jwt.verify(token,process.env.JWT_SECRET_KEY)
+        const user = await userModel.findOne({email:verifiedUser.email})
+        
+        res.status(200).json({
+            success:true,
+            user:user
+        })
+        next()
+    }
 }
 
 
@@ -57,5 +81,6 @@ const logoutUser = async (req,res)=>{
 module.exports =  {
     loginUser,
     registerUser,
-    logoutUser
+    logoutUser,
+    checkAuth
 }
