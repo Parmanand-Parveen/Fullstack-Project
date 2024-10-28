98913ed9c8910945bbcaeaf55eb51b234f03b102
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-28 19:51:21 +0530

    User Router✅

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,10 +1,11 @@
-const {registerUser,loginUser,logoutUser} = require("../controllers/user.control")
+const {registerUser,loginUser,logoutUser, checkAuth} = require("../controllers/user.control")
 const Router = require("express")
 
 const router = Router()
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/logout").post(logoutUser); 
+router.route("/logout").get(logoutUser); 
+router.route("/checkAuth").get(checkAuth);
 
 module.exports = router
