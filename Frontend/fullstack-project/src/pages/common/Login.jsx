cd5eff2bcb8d491d9709175d682a7937f43d658d
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-11-02 17:03:02 +0530

    Login + Signup page ✅

--- Frontend/fullstack-project/src/pages/common/Login.jsx
+++ Frontend/fullstack-project/src/pages/common/Login.jsx
@@ -1,20 +1,36 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import React, { useState } from "react";
+import { toast, useToast } from "@/hooks/use-toast";
+import { loginhandeler } from "@/Store/auth-slice";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const {toast} = useToast()
+  const dispatch = useDispatch()
   const initialFormData = {
     email: "",
     password: "",
   };
 
+
   const [formData, setFormData] = useState(initialFormData);
 
   const loginhandel = (e) => {
     e.preventDefault();
-    console.log(formData);
+    dispatch(loginhandeler(formData)).then((res) => {
+      toast({
+        title: "Success",
+        description: "Login Success",
+        variant: "default",
+        isClosable: true,
+      })
+      window.location.reload();
+    })
+    setFormData(initialFormData); 
+    
   };
 
   return (
@@ -22,6 +38,8 @@ function Login() {
       <form on onSubmit={loginhandel} className="mt-10">
         <Label htmlFor="email">Email</Label>
         <Input
+        value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
           className="bg-transparent mb-3 w-96"
           placeholder="email"
           type="email"
@@ -29,13 +47,15 @@ function Login() {
         />
         <Label htmlFor="password">Password</Label>
         <Input
+        value={formData.password}
+        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
           className="bg-transparent mb-3 w-96"
           placeholder="password"
           type="password"
           id="password"
         />
         <Button type="submit" className="w-96 bg-blue-600">
-          {" "}
+
           Login
         </Button>
         <p className="text-sm">
--- Frontend/fullstack-project/src/pages/common/SignUp.jsx
+++ Frontend/fullstack-project/src/pages/common/SignUp.jsx
@@ -1,25 +1,73 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { registerHandel } from "@/Store/auth-slice/index.js";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
 
 function SignUp() {
-  return (
-    <div className='w-full flex justify-center items-center'>
-       <form  className='mt-10'>
-         <Label htmlFor='username'>Username</Label>
-         <Input className="bg-transparent mb-3 w-96" placeholder='username' type="text" id='username' />
-         <Label htmlFor='email'>Email</Label>
-         <Input className="bg-transparent mb-3 w-96" placeholder='email'  type="email" id='email' />
-         <Label htmlFor='password'>Password</Label>
-         <Input className="bg-transparent mb-3 w-96" placeholder='password'  type="password" id='password' />
-         <Button className="w-96 bg-blue-600">Sign Up</Button>
-         <p className='text-sm'>Already have an account? <Link to="/auth/login">Login</Link></p>
-        </form>
+const navigate = useNavigate()
+const dispatch = useDispatch()
 
+  const initialFormData = {
+    username: "",
+    email: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(initialFormData);
+
+  async function  handelForm(e) {
+    e.preventDefault();
+      const res =  dispatch(registerHandel(formData)).then((res) => {
+        window.location.reload();
+        console.log(res)
+      })
+       console.log(res)
+    setFormData(initialFormData);
+  };
+
+
+  return (
+    <div className="w-full flex justify-center items-center">
+      <form onSubmit={handelForm} className="mt-10">
+        <Label htmlFor="username">Username</Label>
+        <Input
+          value={formData.username}
+          onChange={(e) =>
+            setFormData({ ...formData, username: e.target.value })
+          }
+          className="bg-transparent mb-3 w-96"
+          placeholder="username"
+          type="text"
+          id="username"
+        />
+        <Label htmlFor="email">Email</Label>
+        <Input
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          className="bg-transparent mb-3 w-96"
+          placeholder="email"
+          type="email"
+          id="email"
+        />
+        <Label htmlFor="password">Password</Label>
+        <Input
+          value={formData.password}
+          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+          className="bg-transparent mb-3 w-96"
+          placeholder="password"
+          type="password"
+          id="password"
+        />
+        <Button type="submit" className="w-96 bg-blue-600">Sign Up</Button>
+        <p className="text-sm">
+          Already have an account? <Link to="/auth/login">Login</Link>
+        </p>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
