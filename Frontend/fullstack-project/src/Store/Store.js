f3364f4c4826fc54412ae0674b91bd77e61aae52
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-11-07 06:46:46 +0530

    Store ✅

--- Frontend/fullstack-project/src/Store/Store.js
+++ Frontend/fullstack-project/src/Store/Store.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit" 
 import authReducer from "./auth-slice";
+import productReducer from "./admin/product"
+import shopReducer from "./shop/shopProduct"
+import cartReducer from "./shop/cart"
 
 
 const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        product: productReducer,
+        shopingProduct :shopReducer,
+        userCart:cartReducer
     }
 })
 
