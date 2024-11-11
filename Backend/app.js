c3b6a15163dfd45933aed5596f00c47ab4a94599
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-11-11 08:39:34 +0530

    Backend ✅

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,34 @@
 const express = require("express")
 const userRouter = require("./routes/user.routes")
+const productRouter = require("./routes/admin/product.routes")
+const shopProductRouter = require("./routes/shop/product.routes")
+const cartRoute = require("./routes/shop/cart.routes")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
 const dotenv= require("dotenv").config()
+const cors = require("cors")
 
 const  {MONGO_DB_SERVER} = process.env
 
 const app = express()
-console.log(process.env)
+
+
+
+
+// app.use(
+//     cors({
+//       origin: "http://localhost:5173",
+//       methods: ["GET", "POST", "DELETE", "PUT"],
+//       allowedHeaders: [
+//         "Content-Type",
+//         "Authorization",
+//         "Cache-Control",
+//         "Expires",
+//         "Pragma",
+//       ],
+//       credentials: true,
+//     })
+//   );
 
 
 app.use(express.json({limit: "16kb"}))
@@ -20,7 +41,18 @@ mongoose.connect(MONGO_DB_SERVER)
 
 
 app.use("/api/v1",userRouter)
+app.use("/api/v1",productRouter)
+app.use("/api/v1",shopProductRouter)
+app.use("/api/v1",cartRoute)
 
 
-// http://localhost:3000/api/v1/register
-app.listen(3000)
+
+app.listen(process.env.PORT,(error) => {
+ if(!error){
+    console.log("listening on port 3000")
+ }else{
+    console.log(error)
+ }
+
+}
+)
