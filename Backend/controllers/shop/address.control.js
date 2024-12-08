b6e6c7c01a5406e385019dc9854be328ee7c1836
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-12-08 11:02:24 +0530

    Address Controller ✅

---
+++ Backend/controllers/shop/address.control.js
@@ -0,0 +1,57 @@
+const addressModel = require("../../models/address.model")
+const userModel = require("../../models/user.model")
+
+
+
+const createAddress = async(req,res)=>{
+    const {userId,address,city,state,country,pincode,phone} = req.body
+     try {
+        const createdAddress = await addressModel.create({userId,address,city,state,country,pincode,phone})
+        res.json({success:true,message:"Address created successfully",createdAddress}).status(202)
+     } catch (error) {
+        if(error){
+            res.json({success:false,message:"Error occured while creating the address"}).status(400)
+        }
+     }
+  
+}
+
+
+const deleteAddress = async(req,res)=>{
+    const {id} = req.params
+    try {
+        const deletedAddress = await addressModel.findByIdAndDelete(id)
+    res.json({success:true,message:"Address deleted successfully",deletedAddress}).status(202)
+    } catch (error) {
+        if(error){
+            res.json({success:false,message:"Error occured while deleting the address"}).status(400)
+        }
+    }
+} 
+
+
+const updateAddress = async(req,res)=>{
+    const {id} = req.params
+    const {address,city,state,country,pincode,phone} = req.body
+    try {
+        const updatedAddress = await addressModel.findByIdAndUpdate(id,{address,city,state,country,pincode,phone},{new:true})
+        res.json({success:true,message:"Address updated successfully",updatedAddress}).status(202)
+    } catch (error) {
+        if(error){
+            res.json({success:false,message:"Error occured while updating the address"}).status(400)
+        }
+    }}
+
+   const fetchAllAddress = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const address = await addressModel.find({ userId });
+      res.json({ success: true, address });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false, message: "Error occured while fetching the address" });
+    }
+   } 
+
+
+module.exports = {createAddress,deleteAddress,updateAddress,fetchAllAddress}
