629cf658c4a63e20c652f07b6232d4e1c63ef301
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-12-08 11:02:09 +0530

    Address model ✅

---
+++ Backend/models/address.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const addressSchema = new Schema({
+  userId: {
+    type:String,
+  },
+  address: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  pincode: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+});
+
+
+module.exports = mongoose.model("Address", addressSchema)
